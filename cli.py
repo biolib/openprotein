8f6e28a3aecb984742bfaf92a2cfb2ab6ab3e232
Jeppe Hallgren <jeppe@hallgren.com>
2018-09-08 17:18:36 +0200

    initial commit

---
+++ cli.py
@@ -0,0 +1,8 @@
+# This file is part of the OpenProtein project.
+#
+# @author Jeppe Hallgren
+#
+# For license information, please see the LICENSE file in the root directory.
+
+print("--- OpenProtein v0.1 ---")
+
