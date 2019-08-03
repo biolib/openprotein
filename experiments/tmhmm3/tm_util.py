4445f4c0be59cde28c405a0d24299a5f56d8581d
Jeppe Hallgren <4094238+JeppeHallgren@users.noreply.github.com>
2019-08-03 19:15:14 +0200

    fix

--- experiments/tmhmm3/tm_util.py
+++ experiments/tmhmm3/tm_util.py
@@ -301,16 +301,13 @@ def original_labels_to_fasta(label_list):
 
 def get_predicted_type_from_labels(labels):
     labels = list([int(i) for i in labels])
-    if len(labels) == 0:
-        write_out("Warning: predicting type 3 because no labels were given")
-        return 3
     if 0 in labels or 1 in labels:
-        if labels[0] == 2:
+        if 2 in labels:
             return 1
         else:
             return 0
     else:
-        if labels[0] == 2:
+        if 2 in labels:
             return 2
         else:
             return 3
